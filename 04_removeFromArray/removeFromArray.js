306873ec3ab555af6a6646c6caafcd09e1e25588
Danila Kritsky <danilakritsky@gmail.com>
2022-07-19 11:53:37 +0000

    Complete task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  
+  for (const item of args) {
+    const index = array.indexOf(item);
+    if (index != -1) {
+      array.splice(index, 1);
+    }
+  }
+  return array
 };
 
 // Do not edit below this line
